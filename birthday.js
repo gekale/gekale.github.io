cb3e67637b9a878390eaaae978fdc2c8239e088b
alternativebyte <131468696+alternativebyte@users.noreply.github.com>
2024-08-07 03:55:34 +0000

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -1,7 +1,7 @@
-        var today = new Date();
-        var day = today.getDate();
-        var month = today.getMonth() + 1;
+var today = new Date();
+var day = today.getDate();
+var month = today.getMonth() + 1;
 
-        if (day === 7 && month === 8) {
-            document.getElementById('header').innerText = "IT IS TODAY GEKALE'S BIRTHDAY!!!!";
-        }
+if (day === 2 && month === 8) {
+        document.getElementById('header').innerText = "IT IS TODAY GEKALE'S BIRTHDAY!!!!";
+}
