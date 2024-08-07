ab455df5a8877592de6e4464079191849e51f5bd
alternativebyte <131468696+alternativebyte@users.noreply.github.com>
2024-08-07 03:54:12 +0000

    Create birthday.js

---
+++ birthday.js
@@ -0,0 +1,7 @@
+        var today = new Date();
+        var day = today.getDate();
+        var month = today.getMonth() + 1;
+
+        if (day === 2 && month === 8) {
+            document.getElementById('header').innerText = "IT IS TODAY GEKALE'S BIRTHDAY!!!!";
+        }
