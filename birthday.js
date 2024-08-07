5c68ffe08a28000713ad1d959b804a6da80b6e82
alternativebyte <131468696+alternativebyte@users.noreply.github.com>
2024-08-07 04:34:20 +0000

    sucessful shorten

--- birthday.js
+++ birthday.js
@@ -1,2 +1,2 @@
 var today = new Date();
-if (today.getDate() === 7 && today.getMonth() + 1 === 8) {document.getElementById('header').innerText = "IT IS TODAY GEKALE'S BIRTHDAY!!!!"}
+if (today.getDate() === 2 && today.getMonth() + 1 === 8) {document.getElementById('header').innerText = "IT IS TODAY GEKALE'S BIRTHDAY!!!!"}
