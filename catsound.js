3197b2333a0e79e453d8f9ff00c90d18120cfb84
catty <105548596+gekale@users.noreply.github.com>
2024-08-08 03:54:06 +0300

    Create catsound.js

---
+++ catsound.js
@@ -0,0 +1,3 @@
+document.getElementById('cat-sound-button').addEventListener('click', function() {
+var catSound = new Audio('https://www.myinstants.com/media/sounds/cat-meow.mp3');
+catSound.play();
