57947342fd7fae94e3c8c4d80382cf4bfd948411
catty <105548596+gekale@users.noreply.github.com>
2024-08-08 04:03:08 +0300

    Delete catsound.js

--- catsound.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('cat-sound-button').addEventListener('click', function() {
-var catSound = new Audio('https://www.myinstants.com/media/sounds/cat-meow.mp3');
-catSound.play();
