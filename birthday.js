99e9e1e3e70221ebefc7e1ef02d976ff3ce77a0a
catty <105548596+gekale@users.noreply.github.com>
2024-08-08 04:11:31 +0300

    Delete birthday.js

--- birthday.js
+++
@@ -1,7 +0,0 @@
-var today = new Date();
-var day = today.getDate()
-var month = today.getMonth() + 1
-if (day == 2 && month == 8) 
-{
-document.getElementById('header').innerText = "IT IS TODAY GEKALE'S BIRTHDAY!!!!"
-}
