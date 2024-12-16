935e7140fb4454790edb842dc8ccbf5e8c09f13c
catty <105548596+gekale@users.noreply.github.com>
2024-12-16 17:49:38 +0200

    Create birthday.js

---
+++ birthday.js
@@ -0,0 +1,8 @@
+    <script>
+        var today = new Date();
+        var day = today.getDate()
+        var month = today.getMonth() + 1
+        if (day == 2 && month == 8) {
+            document.getElementById('header').innerText = "IT IS TODAY GEKALE'S BIRTHDAY!!!!"
+        }
+    </script>
