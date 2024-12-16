47535f9ae7e2134f9f25905ff808c28120f0652b
catty <105548596+gekale@users.noreply.github.com>
2024-12-16 17:53:09 +0200

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -1,6 +1,9 @@
-var today = new Date();
-var day = today.getDate();
-var month = today.getMonth() + 1;
-if (day == 2 && month == 8) {
-    document.getElementById('header').innerText = "It is Gekara's Birthday!";
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+
+    if (day === 2 && month === 8) {
+        document.getElementById("header").innerText = "It is Gekara's Birthday!";
+    }
+});
